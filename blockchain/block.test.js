2e96bc980759d59081a4be25b3e88ed0a0995f77
Hakan <hakankosanoglu@hotmail.com>
2021-02-23 10:16:05 +0300

    Add: Proof of Work

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -16,4 +16,17 @@ describe('Block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash)
     })
 
+    it('generates a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty))
+        console.log(block.toString())
+    })
+
+    it('lowers the diffuculty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+360000)).toEqual(block.difficulty-1)
+    })
+
+    it('raises the diffuculty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+2)).toEqual(block.difficulty+1)
+    })
+
 })
